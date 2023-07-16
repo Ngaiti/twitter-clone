45e1f59503d36667310f2ef4c9a1c5b7dca0bb36
Ngaiti <henfanar@gmail.com>
2023-07-16 23:24:55 +0800

    Login Signup successful

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,7 +1,48 @@
+import { useState } from "react";
 import { Button, Col, Image, Row, Modal, Form } from "react-bootstrap";
+import axios from "axios"
+import useLocalStorage from "use-local-storage";
+
+
 
 export default function AuthPage() {
     const loginImage = "https://sig1.co/img-twitter-1";
+    const url = "https://auth-back-end-ngaiti.sigma-school-full-stack.repl.co"
+
+    const [modalShow, setModalShow] = useState(null);
+    const handleShowSignUp = () => setModalShow("SignUp");
+    const handleShowLogin = () => setModalShow("Login");
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false)
+    const handleShow = () => setShow(true)
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [authToken, setAuthToken] = useLocalStorage("authToken", "");
+
+
+    const handleSignUp = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(`${url}/signup`, { username, password });
+            console.log(res.data);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        try {
+            const res = await axios.post(`${url}/login`, { username, password });
+            if (res.data && res.data.auth === true && res.data.token) {
+                setAuthToken(res.data.token); // Save token to localStorage.
+                console.log("Login was successful, token saved");
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     return (
         <Row>
             <Col sm={6}>
@@ -27,7 +68,7 @@ export default function AuthPage() {
                         <i className="bi bi-apple"></i> Sign up with Apple
                     </Button>
                     <p style={{ textAlign: "center" }}>or</p>
-                    <Button className="rounded-pill">
+                    <Button className="rounded-pill" onClick={handleShowSignUp}>
                         Create an account
                     </Button>
                     <p style={{ fontSize: "12px" }}>
@@ -41,11 +82,60 @@ export default function AuthPage() {
                     <Button
                         className="rounded-pill"
                         variant="outline-primary"
+                        onClick={handleShowLogin}
                     >
                         Sign in
                     </Button>
                 </Col>
+                <Modal
+                    show={modalShow !== null}
+                    onHide={handleClose}
+                    animation={false}
+                    centered
+                >
+                    <Modal.Body>
+                        <h2 className="mb-4" style={{ fontWeight: "bold" }}>
+                            {modalShow === "SignUp"
+                                ? "Create your account"
+                                : "Log in to your account"}
+                        </h2>
+                        <Form
+                            className="d-grid gap-2 px-5"
+                            onSubmit={modalShow === "SignUp" ? handleSignUp : handleLogin}
+                        >
+                            <Form.Group className="mb-3" controlId="formBasicEmail">
+                                <Form.Control
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    type="email"
+                                    placeholder="Enter username"
+                                />
+                            </Form.Group>
+
+                            <Form.Group className="mb-3" controlId="formBasicPassword">
+                                <Form.Control
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    type="password"
+                                    placeholder="Password"
+                                />
+                            </Form.Group>
+                            <p style={{ fontSize: "12px" }}>
+                                By signing up, you agree to the Terms of Service and Privacy
+                                Policy, including Cookie Use. SigmaTweets may use your contact
+                                information, including your email address and phone number for
+                                purposes outlined in our Privacy Policy, like keeping your
+                                account secure and personalising our services, including ads.
+                                Learn more. Others will be able to find you by email or phone
+                                number, when provided, unless you choose otherwise here.
+                            </p>
+
+                            <Button className="rounded-pill" type="submit">
+                                {modalShow === "SignUp" ? "Sign up" : "Log in"}
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
             </Col>
         </Row>
+
     )
 }
