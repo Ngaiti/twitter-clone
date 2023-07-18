ce762b7ad866e534d8c38dcc0081719f910b1607
Ngaiti <henfanar@gmail.com>
2023-07-18 11:27:17 +0800

    likes added

--- src/components/ProfilePostCard.jsx
+++ src/components/ProfilePostCard.jsx
@@ -1,9 +1,20 @@
 import { Button, Col, Image, Row } from "react-bootstrap";
+import { useState, useEffect } from "react";
 
-export default function ProfilePostCard({ content }) {
+export default function ProfilePostCard({ content, postId }) {
+    const [likes, setLikes] = useState(0);
+
     const pic =
         "https://pbs.twimg.com/profile_images/1587405892437221376/h167Jlb2_400x400.jpg";
 
+    useEffect(() => {
+        fetch(
+            `https://twitter-api-ngaiti.sigma-school-full-stack.repl.co/likes/post/${postId}`
+        )
+            .then((response) => response.json())
+            .then((data) => setLikes(data.length))
+            .catch((error) => console.error("Error:", error));
+    }, [postId]);
 
     return (
         <Row
@@ -28,7 +39,7 @@ export default function ProfilePostCard({ content }) {
                         <i className="bi bi-repeat"></i>
                     </Button>
                     <Button variant="light">
-                        <i className="bi bi-heart"></i>
+                        <i className="bi bi-heart"></i> {likes}
                     </Button>
                     <Button variant="light">
                         <i className="bi bi-graph-up"></i> 61
